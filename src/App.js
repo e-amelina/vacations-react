8ac9b224fb1257a0efa4b12bc56ab8045389c339
Elena Amelina <e.amelina20@gmail.com>
2021-02-26 00:43:21 +0200

    Added request to server

--- src/App.js
+++ src/App.js
@@ -10,15 +10,32 @@ export default class App extends Component {
     super(props);
     this.state = {
       currentDate: new Date(),
-      dataTable: data,
     }
 
     this.nextMonth = this.nextMonth.bind(this);
     this.prevMonth = this.prevMonth.bind(this);
     this.addVacation = this.addVacation.bind(this);
   }
-  
 
+  fetchCalendar() {
+    
+    return fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      method: "PUT",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        this.setState({dataTable: json});
+      }).catch(err => console.log(err));
+  }
+  
+  componentDidMount() {
+    this.fetchCalendar();
+  }
+  
   prevMonth() {
     this.setState({currentDate: new Date(this.state.currentDate.getFullYear(), this.state.currentDate.getMonth() - 1, 1)});
   }
@@ -36,11 +53,15 @@ export default class App extends Component {
   }
 
   render() {
+    
     return (
       <div className="App">
+        {(this.state.dataTable) &&
+        <>
         <Navigation currentDate = {this.state.currentDate} prevMonth = {this.prevMonth} nextMonth = {this.nextMonth}/>
         <Table currentDate = {this.state.currentDate} dataTable = {this.state.dataTable}/>
         <Modal currentDate = {this.state.currentDate} addVacation = {this.addVacation} dataTable = {this.state.dataTable}/>
+        </>}
       </div>
     );
   }
